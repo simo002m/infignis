fb94fdb436676c2c0ff4f4db6f4266e1988b2fa9
Simon Iversen <simo002m@gmail.com>
2017-12-20 14:02:22 +0000

    Initial commit

---
+++ signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Brandsikkerhed</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import the Inconsolata font-->
+	<link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet'>
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+
+
+
+
+	<!--Import jQuery before materialize.js-->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
+        
