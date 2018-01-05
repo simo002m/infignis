78536f7c71ca58dab1a953130d66116c53914c9d
Simon Iversen <simo002m@gmail.com>
2018-01-05 16:58:38 +0100

    Changes made to login/login.php

--- login/login.php
+++ login/login.php
@@ -5,52 +5,80 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
 	<!--Import Google Icon Font-->
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<!--Import the Inconsolata font-->
-	<link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet'>
-	<!--Import materialize.css-->
-	<link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!--Import the Inconsolata font-->
+		<link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet'>
+		<!--Import materialize.css-->
+		<link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
 
-	<!--Let browser know website is optimized for mobile-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-<body>
+	<style>
+	input.fromInvalid::-webkit-input-placeholder {
+		color: red;
+	}
+	</style>
+	</head>
 
-	<div class="container">
+	<body>
+
+		<div class="container">
+			
+			<div class="row"> 
 		
-		<div class="row"> 
-	
-			<div class="col s2"> 
-			</div>
-			<div class="col s8"> 
-				<div class="card orange darken-2 hoverable">
-					<div class="card-content white-text">
-					<h5>Log ind</h5>
-				
-					<form action="login.php" method="post">
-						<div class="input-field">
-							<input type="text" placeholder="Brugernavn" name="username" class="validate">
-							<div class="input-field">
-							<input type="password" name="password" placeholder="Adgangskode" class="validate">	
-					<button type="submit" class="btn red lighten-1 col s12">Log ind</button>
-						</div>
-					</form>
-
+				<div class="col s2"> <!--Center allignment stuff--> 
 				</div>
+				<div class="col s8"> 
+					<div class="card orange darken-2 hoverable">
+						<div class="card-content white-text">
+						<h5>Log ind</h5>
+					
+						<form method="post" action="login.php">
+							<div class="input-field">
+								<input id="username_input" type="text" placeholder="Brugernavn" name="username" class="validate">
+								<div class="input-field">
+								<input id="password_input" type="password" name="password" placeholder="Adgangskode" class="validate">	
+
+							<button type="submit" class="btn red lighten-1 col s12">Log ind</button>
+							</div>
+						</form>
+
+					</div>
+				</div>
+			
+				<div class="col s2"> <!--Center allignment stuff--> 
+				</div>
+			
 			</div>
-		
-			<div class="col s2">
-			</div>
-		
+
 		</div>
 
-	</div>
-
 <?php
+	# define variables and set to empty values
+	$usernamename = $password = "";
 
-$target_file = fopen("users", "r");
-echo fgets($target_file);
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["username"])) { # check wether the value of the key 'username' is empty or not
+			echo "<script>var username_input = document.getElementById('username_input');username_input.placeholder = 'This field cant be empty';</script>"; # error message
+	} else {
+		$username = test_input($_POST["username"]);
+	}
+
+	if (empty($_POST["password"])) { # check wether the value of the key 'password' is empty or not
+		echo "<script>var password_input = document.getElementById('password_input'); password_input.placeholder = 'This field cant be empty';</script>"; # error message
+	} else {
+		$password = test_input($_POST["password"]);
+	}
+}
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 
 ?>
 
--- services/uf.php
+++ services/uf.php
@@ -1,25 +1,86 @@
+¨
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Infignis - Haltegning</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <head>
+        <title>Infignis - UF Haltegning</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
-	<!--Import Google Icon Font-->
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<!--Import the Inconsolata font-->
-	<link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet'>
-	<!--Import materialize.css-->
-	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import the Inconsolata font-->
+        <link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet'>
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-	<!--Let browser know website is optimized for mobile-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
 
-<body>
 
-<?php
+    <body>
 
-?>
+        <form action="uf.php" method="get">
+            <input type="hidden" name="addBoks" value="true">
+            <input type="submit" value="Run me now!">
+        </form>
 
-</body>
+        <h1>Skriver</h1>
+
+
+
+        <?php
+
+        //OBJEKTER
+        class infignisBoks
+        {
+
+            function retData($flameSensor, $tempSensor){
+                
+                
+                
+                echo "From infignisBoks<br>";
+                echo "Flame = " . $flameSensor . "<br>";
+                echo "Temp = " . $tempSensor . "<br>";
+                echo "retData function end<br>";
+
+            }
+        }
+
+        $boksSens = new infignisBoks;
+
+
+        $flame = $_GET["flame"];
+        $temp = $_GET["temp"];
+
+        $boksSens->retData($flame, $temp);
+
+        echo "<br>";
+
+        echo "Flame sensor = " . $flame;
+        echo "<br>";
+        echo "Temperatur sensor = " . $temp;
+        echo "<br>";
+
+
+        $myfile = fopen("sensorLog.txt", "r+") or die("Unable to open file!");
+        fwrite($myfile, "flame=");
+        fwrite($myfile, $flame);
+        fwrite($myfile, "\n");
+        fwrite($myfile, "temp=");
+        fwrite($myfile, $temp);
+        
+        $logText = fread($myfile);
+        echo "Log Text = " . $logText;
+
+        fclose($myfile);
+
+
+
+
+
+        ?>
+
+    </body>
 </html>
+
+
